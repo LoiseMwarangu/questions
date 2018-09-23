74abd4e76da0a609d8f6b87b046cd03362a3fbc4
Loise Mwarangu <loisemwarangu@gmail.com>
2018-09-23 22:38:21 +0300

    added j query to js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,7 @@
- $(document).ready(function() {
-  $(".radio").submit(function(event) {
-  event.preventDefault();
-   var button1 = document.getElementById("button1");
-   var button2 = document.getElementById("button2");
-   if (button1.checked){
-     alert ("your score is 0");
-   }
-   else if (button2.checked) {
-   alert ("your score is 20");
- }
- )}
- )}
+$(document).ready(function() {
+ $("form#score").submit(function(event) {
+   event.preventDefault();
+ var scores =$("input#radio").val();
+ var myAnswers = ["yes","noneOfTheAbove","true","yeah","netscapeCommunications","allTheAbove","yess"],
+
+ 
